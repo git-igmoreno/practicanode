f9b756635a8ccb2cc5f7ca372f1b472eced3f4e5
Fco javier Ribo labrador <elribonazo@gmail.com>
2020-10-03 19:30:03 +0200

    rutas

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ app.get('/',function(req,res){
     res.status(200);
 });
 
+app.post('/',function(req,res){
+    res.json({
+        success:true
+    })
+    res.status(200);
+});
+
+
+app.put('/',function(req,res){
+    res.json({
+        success:true
+    })
+    res.status(200);
+});
+
+
+app.delete('/',function(req,res){
+    res.json({
+        success:true
+    })
+    res.status(200);
+});
+
+
 
 var server = app.listen(3000,function() {});
 
