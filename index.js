38e8f27c865e5ffa8cf9929696717e6223565289
Fco javier Ribo labrador <elribonazo@gmail.com>
2020-10-03 19:49:47 +0200

    mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express=require('express');
 var app=express();
 
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieparser = require('cookie-parser');
 const expressSanitizer = require('express-sanitizer');
@@ -11,7 +12,16 @@ app.use(bodyParser.json({ limit: '200mb' }))
 app.use(bodyParser.urlencoded({ extended: false, limit: '200mb' }))
 app.use(expressSanitizer())
 
+/**
+ * Modelos
+ */
+const Producto = mongoose.model('Kitten', new mongoose.Schema({
+    name: String
+}));
 
+/**
+ * Controladores
+ */
 app.get('/',function(req, res){
     res.json({
         success:true
@@ -43,14 +53,12 @@ app.delete('/',function(req,res){
     res.status(200);
 });
 
-
-
-
 const puerto = 3000;
-var server = app.listen(puerto, function() {
-
-    console.log("Servidor iniciado en " + puerto)
-
+mongoose.connect('mongodb+srv://root:ycyeWmjZbJrnhmLf@cluster0.mibrz.mongodb.net/practicanode?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
+    if (err) {
+        throw new Error(err.message);
+    }
+    app.listen(puerto, function() {
+        console.log("Servidor iniciado en " + puerto);
+    });
 });
-
-
